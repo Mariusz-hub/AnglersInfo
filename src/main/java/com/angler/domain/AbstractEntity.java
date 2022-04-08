e678dc8cd89c537837485ece22b93b179d934149
Mariusz <mariusz_podsiadlly@o2.pl>
2022-04-08 19:28:28 +0200

    Fish entity

--- src/main/java/com/angler/domain/AbstractEntity.java
+++ src/main/java/com/angler/domain/AbstractEntity.java
@@ -1,6 +1,7 @@
 package com.angler.domain;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @MappedSuperclass
 public class AbstractEntity {
@@ -9,4 +10,32 @@ public class AbstractEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID")
     private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractEntity)) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractEntity{" +
+                "id=" + id +
+                '}';
+    }
 }
--- src/main/java/com/angler/domain/Fish.java
+++ src/main/java/com/angler/domain/Fish.java
@@ -1,4 +1,77 @@
 package com.angler.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "fish")
 public class Fish extends AbstractEntity {
+
+    @Column(nullable = false)
+    private String fishName;
+    @Column(nullable = false)
+    private int protectionSize;
+    @Column(nullable = false)
+    private LocalDate periodOfProtection;
+
+    public Fish() {
+    }
+
+    public Fish(String fishName, int protectionSize, LocalDate periodOfProtection) {
+        this.fishName = fishName;
+        this.protectionSize = protectionSize;
+        this.periodOfProtection = periodOfProtection;
+    }
+
+    public String getFishName() {
+        return fishName;
+    }
+
+    public void setFishName(String fishName) {
+        this.fishName = fishName;
+    }
+
+    public int getProtectionSize() {
+        return protectionSize;
+    }
+
+    public void setProtectionSize(int protectionSize) {
+        this.protectionSize = protectionSize;
+    }
+
+    public LocalDate getPeriodOfProtection() {
+        return periodOfProtection;
+    }
+
+    public void setPeriodOfProtection(LocalDate periodOfProtection) {
+        this.periodOfProtection = periodOfProtection;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Fish)) return false;
+        if (!super.equals(o)) return false;
+        Fish fish = (Fish) o;
+        return protectionSize == fish.protectionSize &&
+                Objects.equals(fishName, fish.fishName) &&
+                Objects.equals(periodOfProtection, fish.periodOfProtection);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), fishName, protectionSize, periodOfProtection);
+    }
+
+    @Override
+    public String toString() {
+        return "Fish{" +
+                "fishName='" + fishName + '\'' +
+                ", protectionSize=" + protectionSize +
+                ", periodOfProtection=" + periodOfProtection +
+                '}';
+    }
 }
