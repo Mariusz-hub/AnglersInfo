fd1711e43ccaf86ad158b66a08545ab1f6e1d05e
Mariusz <mariusz_podsiadlly@o2.pl>
2022-04-08 18:51:40 +0200

    Add domain

---
+++ src/main/java/com/angler/AnglersInfoApplication.java
@@ -0,0 +1,13 @@
+package com.angler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnglersInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnglersInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angler/AnglersInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnglersInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
